ababaeb7a52e7cc19e5b727dcde750cf9e8d5ee5
chenjef <1737752975@qq.com>
2015-12-30 18:23:38 +0800

    'end'

--- js/backstage.js
+++ js/backstage.js
@@ -1,16 +1,25 @@
 function Stage(){
-	this.myProto();
-}
-Stage.prototype.myProto=function(){
-
+	this.myClick();
+	this.lock = false;
+};
+Stage.prototype.myClick=function(){
 	$('.header .photo').click(function(){ /*侧滑栏*/
 		$('.container').toggleClass('addAni');
 		$('.sideNav').toggleClass('reduAni');
 	});
+	var that = this;
+	setInterval(function(){  //设置锁  
+		that.lock = false;
+	},1000);
 
-	$('.side').click(function(){
-		$('.sideNav ul').hide(500);
-		$(this).next().slideToggle(500);
+	$('.side').click(function(){  //侧栏下拉
+		if(that.lock != true){
+			console.log(that.lock);
+			$('.sideNav ul').hide(500);
+			$(this).next().slideToggle(500);
+		}	
+		that.lock = true;
 	});
 
-}
+
+};
