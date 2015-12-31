1885a59844be95228a31f03b04c57424fc234d2a
chenjef <1737752975@qq.com>
2015-12-31 17:20:33 +0800

    'fangjiale'

--- js/backstage.js
+++ js/backstage.js
@@ -21,23 +21,27 @@ Stage.prototype.myClick=function(){
 	});
 
 	$('.testClick').click(function(){ //测试点击侧边栏的选项
+		$(".tbody table").html('');
 		$.ajax({
 			type:'GET',
 			url:'test.json',
 			dataType:'json',
 			success:function(data){
 				$.each(data,function(i,n){
-					console.log(n);
 					console.log(i);
-					for(var j = 0; j < n.length; j++){
+					console.log(n);
+					console.log(n.item);
+					var item = n.item;  //更改json数据的项
+					for(var j = 0; j < item.length; j++){
 						var nl = '<tr>';  //一条数据
 							nl += '<td class="radio">';
 							nl += '<input type="radio">';
 							nl += '</td>';
-							nl += '<td class="tablenomal">'+n[j].first+'</td>';
-							nl += '<td class="tablenomal">'+n[j].seccond+'</td>';
+							nl += '<td class="tablenomal">'+item[j].first+'</td>';
+							nl += '<td class="tablenomal">'+item[j].seccond+'</td>';
 							nl += '</tr>';
-						$('table').append(nl);
+						
+						$('.tbody table').append(nl);
 					}
 
 				});
@@ -47,6 +51,40 @@ Stage.prototype.myClick=function(){
 			}
 		});
 	});
+
+	$('.testMy').click(function(){ //测试点击侧边栏的选项
+		$(".tbody table").html('');
+		$.ajax({
+			type:'GET',
+			url:'test.json',
+			dataType:'json',
+			success:function(data){
+				$.each(data,function(i,n){
+					console.log(i);
+					console.log(n);
+					console.log(n.tip);
+					var item = n.tip;  //更改json数据的项
+					for(var j = 0; j < item.length; j++){
+						var nl = '<tr>';  //一条数据
+							nl += '<td class="radio">';
+							nl += '<input type="radio">';
+							nl += '</td>';
+							nl += '<td class="tablenomal">'+item[j].first+'</td>';
+							nl += '<td class="tablenomal">'+item[j].seccond+'</td>';
+							nl += '</tr>';
+						
+						$('.tbody table').append(nl);
+					}
+
+				});
+			},
+			error:function(e){
+				console.log('cuole')
+			}
+		});
+	});
+
+
 	$('.thead li').click(function(){  //why not use addClass,beacuser i show
 		$('.thead li').css({
 			marginTop:'0px',
